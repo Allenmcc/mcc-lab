bc41b11f3fea089b7c02c72164a585b942015ece
chunchen.meng <chunchen.meng@yitu-inc.com>
2020-05-09 17:13:36 +0800

    1

--- src/designMode/atguigu/adapter/classadapter/Client.java
+++ src/designMode/atguigu/adapter/classadapter/Client.java
@@ -3,7 +3,7 @@ package designMode.atguigu.adapter.classadapter;
 public class Client {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
 		System.out.println(" === 类适配器模式 ====");
 		Phone phone = new Phone();
 		phone.charging(new VoltageAdapter());
