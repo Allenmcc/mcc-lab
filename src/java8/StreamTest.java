b9e7913d0b4e7800f158eebf60c1e7bfa447780a
chunchen.meng <chunchen.meng@yitu-inc.com>
2020-05-20 17:27:38 +0800

    init

--- src/java8/StreamTest.java
+++ src/java8/StreamTest.java
@@ -92,10 +92,13 @@ public class StreamTest {
                 s.forEach(System.out::println)
         );
 
+//        streamStream.forEach(System.out::println);
+
 
         //flatMap  打平   {a,a,a,b,b,b,c,c,c,d,d} 相当于addAll
         Stream<Character> streamflatMap = list.stream().flatMap(StreamTest::filterCharacter);
         streamflatMap.forEach(System.out::println);
+
     }
 
     public static Stream<Character> filterCharacter(String str) {
@@ -105,6 +108,50 @@ public class StreamTest {
 
         }
         return charlist.stream();
+    }
+
+    @Test
+    public void testFlatMap(){
+        List<String> teamIndia = Arrays.asList("Virat", "Dhoni", "Jadeja");
+        List<String> teamAustralia = Arrays.asList("Warner", "Watson", "Smith");
+        List<String> teamEngland = Arrays.asList("Alex", "Bell", "Broad");
+        List<String> teamNewZeland = Arrays.asList("Kane", "Nathan", "Vettori");
+        List<String> teamSouthAfrica = Arrays.asList("AB", "Amla", "Faf");
+        List<String> teamWestIndies = Arrays.asList("Sammy", "Gayle", "Narine");
+        List<String> teamSriLanka = Arrays.asList("Mahela", "Sanga", "Dilshan");
+        List<String> teamPakistan = Arrays.asList("Misbah", "Afridi", "Shehzad");
+
+        List<List<String>> playersInWorldCup2016 = new ArrayList<>();
+        playersInWorldCup2016.add(teamIndia);
+        playersInWorldCup2016.add(teamAustralia);
+        playersInWorldCup2016.add(teamEngland);
+        playersInWorldCup2016.add(teamNewZeland);
+        playersInWorldCup2016.add(teamSouthAfrica);
+        playersInWorldCup2016.add(teamWestIndies);
+        playersInWorldCup2016.add(teamSriLanka);
+        playersInWorldCup2016.add(teamPakistan);
+
+        // Let's print all players before Java 8
+        List<String> listOfAllPlayers = new ArrayList<>();
+
+        for(List<String> team : playersInWorldCup2016){
+            for(String name : team){
+                listOfAllPlayers.add(name);
+            }
+        }
+
+        System.out.println("Players playing in world cup 2016");
+        System.out.println(listOfAllPlayers);
+
+
+        // Now let's do this in Java 8 using FlatMap
+        //把几个小的list转换到一个大的list  addAll()
+        List<String> flatMapList = playersInWorldCup2016.stream()
+                .flatMap(pList -> pList.stream())
+                .collect(Collectors.toList());
+
+        System.out.println("List of all Players using Java 8");
+        System.out.println(flatMapList);
     }
 
     /**
